b148f8214a2cf5b8d042c880c4bb2bddc44c8d7b
artemrepko <artemrepko@outlook.com>
2014-05-06 04:31:29 +0300

    Hard night_code by Artem ++

---
+++ app/src/main/java/hrquizer/app/DataBaseClasses/CategoryDBOpenHelper.java
@@ -0,0 +1,44 @@
+package hrquizer.app.DataBaseClasses;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+/**
+ * Created by artem on 06.05.14.
+ */
+public class CategoryDBOpenHelper extends SQLiteOpenHelper implements BaseColumns {
+
+    public static int VERSION = 1;
+    public static String DB_NAME = "hrquizerdb.db";
+
+    public static String CATEGORIES = "Categories";
+    public static String CATEGORY_NAME = "categoryname";
+
+    public static String CATEGORY_DB_CREATED = "CREATE TABLE Categories (_id INTEGER PRIMARY KEY AUTOINCREMENT, categoryname TEXT);";
+
+    public CategoryDBOpenHelper(Context context) {
+        super(context, DB_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CATEGORY_DB_CREATED);
+
+        ContentValues values = new ContentValues();
+
+        for (int i=0; i<5; i++) {
+            values.put(CATEGORY_NAME, "Category " + Integer.toString(i+1));
+            db.insert(CATEGORIES, CATEGORY_NAME, values);
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS" + CATEGORIES);
+        onCreate(db);
+    }
+}
